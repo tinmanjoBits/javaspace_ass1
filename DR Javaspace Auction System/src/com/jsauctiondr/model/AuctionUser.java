f9e5813abdc1ffebc0a13f50613ad14f37c36eb6
david <david@ubuntu>
2014-12-02 14:18:24 +0000

    user info

--- DR Javaspace Auction System/src/com/jsauctiondr/model/AuctionUser.java
+++ DR Javaspace Auction System/src/com/jsauctiondr/model/AuctionUser.java
@@ -4,10 +4,22 @@ import net.jini.core.entry.Entry;
 
 
 public class AuctionUser implements Entry {
+	
+	public String username;
+	public String password;
+	public Integer totalBids;
+	public Integer totalLots;
+	
 
 	public AuctionUser() {
 
 		// TODO Auto-generated constructor stub
+	}
+	
+	public AuctionUser(String user, String password){
+		
+		this.username = user;
+		this.password = password;
 	}
 
 }
